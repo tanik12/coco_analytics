33b421c6db46c2e0fb249e1a6fad9f9dfb8ce181
tanik12 <koyu.tani01@g.softbank.co.jp>
2019-12-27 15:47:19 +0900

    [feat]各クラス数を確認するための関数を追加

--- coco_analytics_api.py
+++ coco_analytics_api.py
@@ -5,6 +5,7 @@ import xml.dom.minidom as md
 import numpy as np
 import os
 import shutil
+import matplotlib.pyplot as plt
 
 import sys
 sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
@@ -205,6 +206,44 @@ def train_val_split(coco_info):
     f_train.close()
     f_val.close()
 
+#各ラベル数を確認するためのもの
+def confirm_category_num(coco_info):
+    label_num = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    for item in coco_info:
+        label_flag = [False, False, False, False, False, False, False, False, False, False]
+        category_nums = np.array(item['category_id'])
+        for label in category_nums:
+            if label == 1:
+                label_flag[0] = True
+            elif label == 2:
+                label_flag[1] = True
+            elif label == 3:
+                label_flag[2] = True
+            elif label == 4:
+                label_flag[3] = True
+            elif label == 6:
+                label_flag[4] = True
+            elif label == 8:
+                label_flag[5] = True
+            elif label == 10:
+                label_flag[6] = True
+            elif label == 16:
+                label_flag[7] = True
+            elif label == 17:
+                label_flag[8] = True
+            elif label == 18:
+                label_flag[9] = True
+         
+        ids = np.where(label_flag)
+        for i in ids:
+            label_num[ids] += 1
+        
+    print(label_num)
+    left = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    label_name = ["person", "bicycle", "car", "motorbike", "bus", "truck", "traffic light", "bird", "cat", "dog"]
+    plt.bar(left, label_num, tick_label=label_name, align="center")
+    plt.show()
+
 if __name__ == "__main__":
     #Ubuntu用
     coco_json_path = "/home/gisen/data/coco/annotations/annotations/instances_train2014.json" 
@@ -224,3 +263,4 @@ if __name__ == "__main__":
     one_detection_debug()
     #specified_num_detection_debug(coco_info)
     train_val_split(coco_info)
+    confirm_category_num(coco_info)
