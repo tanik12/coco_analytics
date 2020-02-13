436683e1609880bd7812d28b25707f75ce06c1e0
tanik12 <koyu.tani01@g.softbank.co.jp>
2020-02-13 20:26:18 +0900

    [update]birdクラスを抽出しないように変更

--- coco_analytics_api.py
+++ coco_analytics_api.py
@@ -216,10 +216,13 @@ def train_val_split(coco_info):
 
 #各ラベル数を確認するためのもの
 def confirm_category_num(coco_info):
-    label_num = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    label_num = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0])
+    ###label_num = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
     for item in coco_info:
-        label_flag = [False, False, False, False, False, False, False, False, False, False]
+        label_flag = [False, False, False, False, False, False, False, False, False]
+        ###label_flag = [False, False, False, False, False, False, False, False, False, False]
         category_nums = np.array(item['category_id'])
+        print("AAAAAAAAAAAA: ", category_nums)
         for label in category_nums:
             if label == 1:
                 label_flag[0] = True
@@ -235,20 +238,22 @@ def confirm_category_num(coco_info):
                 label_flag[5] = True
             elif label == 10:
                 label_flag[6] = True
-            elif label == 16:
-                label_flag[7] = True
+            ###elif label == 16:
+            ###    label_flag[7] = True
             elif label == 17:
-                label_flag[8] = True
+                label_flag[7] = True
             elif label == 18:
-                label_flag[9] = True
+                label_flag[8] = True
          
         ids = np.where(label_flag)
         for i in ids:
             label_num[ids] += 1
         
     print(label_num)
-    left = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-    label_name = ["person", "bicycle", "car", "motorbike", "bus", "truck", "traffic light", "bird", "cat", "dog"]
+    #left = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    #label_name = ["person", "bicycle", "car", "motorbike", "bus", "truck", "traffic light", "bird", "cat", "dog"]
+    left = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    label_name = ["person", "bicycle", "car", "motorbike", "bus", "truck", "traffic light", "cat", "dog"]
     plt.bar(left, label_num, tick_label=label_name, align="center")
     plt.show()
 
@@ -272,14 +277,16 @@ if __name__ == "__main__":
     #coco_json_path = "/Users/gisen/data/coco/annotations/annotations/instances_train2014.json" 
     #coco_img_path = "/Users/gisen/data/coco/images/train2014/"
 
-    categories = [1, 2, 3, 4, 6, 8, 10, 16, 17, 18]
-    label_dic = {1:"person", 2:"bicycle", 3:"car", 4:"motorbike", 6:"bus", 8:"truck", 10:"traffic light", 16:"bird", 17:"cat", 18:"dog"}
+    categories = [1, 2, 3, 4, 6, 8, 10, 17, 18]
+    label_dic = {1:"person", 2:"bicycle", 3:"car", 4:"motorbike", 6:"bus", 8:"truck", 10:"traffic light", 17:"cat", 18:"dog"}
+    #categories = [1, 2, 3, 4, 6, 8, 10, 16, 17, 18]
+    #label_dic = {1:"person", 2:"bicycle", 3:"car", 4:"motorbike", 6:"bus", 8:"truck", 10:"traffic light", 16:"bird", 17:"cat", 18:"dog"}
 
     cofirm_dir()
     coco_info = get_ob_info(coco_json_path, categories)
     make_xml(coco_info, label_dic)
     make_img(coco_img_path, coco_info)
-    one_detection_debug()
+    #one_detection_debug()
     #specified_num_detection_debug(coco_info)
     train_val_split(coco_info)
     confirm_category_num(coco_info)
